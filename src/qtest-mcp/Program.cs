064fbc55383b1e96f0cd8278c1691edd30f5bd25
Kiryl Bahdanovich <work.kiryl.bahdanovich@gmail.com>
2025-08-01 07:37:26 +0200

    fixed qtest mcp

--- src/qtest-mcp/Program.cs
+++ src/qtest-mcp/Program.cs
@@ -17,7 +17,7 @@ builder.Services.AddSingleton(_ =>
 
     // Create an HttpClient with the base address set to the JIRA host
     var client = new HttpClient() { BaseAddress = new Uri(host) };
-    client.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("itbusina/qtest-mcp", "1.0"));
+    client.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("itbusina-qtest-mcp", "1.0"));
     client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
     // Add Bearer Authentication using environment variable QTEST_TOKEN
